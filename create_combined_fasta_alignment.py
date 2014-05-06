e50449345150ab32db25bb01d2265992890062da
Chad Laing <chadlaing@gmail.com>
2014-05-06 15:36:15 -0600

    Final version of alignment combiner; requires output directory as second parameter

--- create_combined_fasta_alignment.py
+++ create_combined_fasta_alignment.py
@@ -1,6 +1,5 @@
 #!/usr/env python
 
-#from subprocess import call
 import argparse
 import os
 
@@ -10,6 +9,7 @@ import os
 parser = argparse.ArgumentParser()
 parser.add_argument("aln_dir", help="The directory where the \
     single gene alignment files are stored")
+parser.add_argument("out_dir", help="Output directory for combined aln file")
 
 args = parser.parse_args()
 print(args.aln_dir)
@@ -18,19 +18,19 @@ genome_names = {}
 
 for file in os.listdir(args.aln_dir):
     if file.endswith(".aln"):
-        print(file)
         in_fh = open(args.aln_dir + file, 'r')
+        print(file)
 
-        current_name = ""
+        current_name = "default"
         for line in in_fh:
             line = line.strip()
             if line[:1] is ">":
-                current_name = line[2:]
+                current_name = line[1:]
                 genome_names.setdefault(current_name, "")
             else:
                 genome_names[current_name] += line
 
-out_fh = open("testout", 'w')
+out_fh = open(args.out_dir, 'w')
 
 for key, value in genome_names.iteritems():
     out_fh.write('>' + key + "\n" + value + "\n")
